d9cc04e20a2c14976105e845891df50a155af055
ParminChickenNugget <71943112+ParminChickenNugget@users.noreply.github.com>
2020-11-14 15:28:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,36 @@ namespace SpriteKind {
     export const Grocery = SpriteKind.create()
     export const CartItem = SpriteKind.create()
 }
-function addToCart () {
-	
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Grocery, function(p: Sprite, g: Sprite) {
+    if(controller.A.isPressed()){
+        addToCart(g)
+        pause(100)
+    }
+})
+function addToCart (grocery: Sprite) {
+	let item = sprites.create(grocery.image, SpriteKind.CartItem)
+    item.follow(player)
+    item.x = player.x
+    item.y = player.y 
 }
-function createProduct () {
-   	
+function createProduct (productImg: Image, cost: number, weight: number, name: string) {
+    product = sprites.create(productImg, SpriteKind.Grocery)
+    sprites.setDataNumber(product, "cost", cost)
+    sprites.setDataNumber(product, "weight", weight)
+    sprites.setDataString(product, "name", name)
+    tiles.placeOnRandomTile(product, myTiles.tile1)
 }
+function creatAllProducts(){
+    for(let i = 0; i < groceryImages.length; i++){
+        let image = groceryImages[i]
+        let cost = groceryCosts[i]
+        let weight = groceryWeights[i]
+        let names = groceryNames[i]
+        createProduct(image, cost, weight, names)
+    }
+
+}
+let product: Sprite = null
 let groceryImages = [
 img`
     . . . 2 2 2 . . . . . . . . . . 
@@ -228,3 +252,22 @@ let player = sprites.create(img`
 controller.moveSprite(player)
 tiles.placeOnTile(player, tiles.getTileLocation(1, 3))
 scene.cameraFollowSprite(player)
+createProduct(img`
+    . . . . . . . e e e e . . . . .
+    . . . . . e e 4 5 5 5 e e . . .
+    . . . . e 4 5 6 2 2 7 6 6 e . .
+    . . . e 5 6 6 7 2 2 6 4 4 4 e .
+    . . e 5 2 2 7 6 6 4 5 5 5 5 4 .
+    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e .
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . .
+    e 5 e c 5 4 5 4 5 5 5 e e . . .
+    e 5 e e 5 5 5 5 5 4 e . . . . .
+    4 5 4 e 5 5 5 5 e e . . . . . .
+    . 4 5 4 5 5 4 e . . . . . . . .
+    . . 4 4 e e e . . . . . . . . .
+`, 1, 3, "test taco")
+creatAllProducts()
